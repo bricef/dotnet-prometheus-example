4e6179cbc58201ea613a3c6e2b84da2affdc4074
Brice Fernandes <brice@weave.works>
2018-07-30 19:01:34 +0100

    Add test endpoitn to generate metrics

--- HomeModule.cs
+++ HomeModule.cs
@@ -1,11 +1,33 @@
+using System;
 using Nancy;
+
 namespace NancyApplication
 {
     public class HomeModule : NancyModule
     {
         public HomeModule()
         {
+            var r = new Random();
             Get("/", args => "Hello World. This is pretty cool!");
+
+            Get("/test",  args => {
+
+                // incur a random delay
+                var sleep_ms = r.Next(2500);
+                System.Threading.Thread.Sleep(sleep_ms);
+
+                // Choose a random return status code
+                var statuses = new int[] {200,200,200,200,200,200,400,400,500};
+                var status = statuses[r.Next(statuses.Length)];
+
+                return Negotiate
+                    .WithStatusCode(status)
+                    .WithModel(new {
+                        message = "Test action",
+                        status = status,
+                        sleep_time_ms = sleep_ms
+                    });
+            });
         }
     }
 }
