af5e538f7f3b0e85cb2810d0eb0a9770e17d563e
Brice Fernandes <brice@weave.works>
2018-07-30 15:10:54 +0100

    Basic nancy project

---
+++ Main.cs
@@ -0,0 +1,21 @@
+using System;
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+namespace NancyApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+            Console.WriteLine("Serving using NancyFx");
+            host.Run();
+        }
+    }
+}
